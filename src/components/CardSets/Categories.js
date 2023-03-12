7d75f031cbf41d7e9a80d7a9dcaf21a2e07d934d
epicX67 <souravgain605@gmail.com>
2023-03-12 21:19:44 +0530

    added categories

--- src/components/CardSets/Categories.js
+++ src/components/CardSets/Categories.js
@@ -1,76 +1,30 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+"use client";
+import { useScrollContainer } from "react-indiana-drag-scroll";
+import { useRouter } from "next/navigation";
 import "./Categories.scss";
 
-import { ScrollMenu } from "react-horizontal-scrolling-menu";
-import { easingFunctions } from "../utils/easingFunctions";
-import { scrollBehavior } from "../utils/scrollBehavior";
-import useDrag from "../utils/useDrag";
-
 export default function Categories({ title, data = [] }) {
-  const navigate = useNavigate();
-  const [duration, setDuration] = React.useState(500);
-  const [ease, setEase] = React.useState("noEasing");
-  const [customAnimation, setCustomAnimation] = React.useState(false);
+  const scrollContainer = useScrollContainer();
+  const router = useRouter();
 
-  function onWheel(apiObj, ev) {
-    const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
-    if (isThouchpad) {
-      ev.stopPropagation();
-      return;
-    }
-    if (ev.deltaY < 0) {
-      // NOTE: for transitions
-      apiObj.scrollNext(undefined, undefined, undefined, { duration });
-    } else if (ev.deltaY > 0) {
-      apiObj.scrollPrev(undefined, undefined, undefined, { duration });
-    }
-  }
-
-  const { dragStart, dragStop, dragMove, dragging } = useDrag();
-  const handleDrag =
-    ({ scrollContainer }) =>
-    (ev) =>
-      dragMove(ev, (posDiff) => {
-        if (scrollContainer.current) {
-          scrollContainer.current.scrollLeft += posDiff;
-        }
-      });
-
-  const [selected, setSelected] = React.useState("");
-  const handleItemClick = (itemId, url) => () => {
-    if (dragging) {
-      return false;
-    }
-    setSelected(selected !== itemId ? itemId : "");
-    navigate(url);
+  const handleItemClick = (url) => () => {
+    router.push(url);
   };
 
   return (
-    <div className="content category">
+    <div className="CardSetTypeA category">
       <p>{title}</p>
-      <ScrollMenu
-        scrollContainerClassName="card-list"
-        LeftArrow={null}
-        RightArrow={null}
-        onWheel={onWheel}
-        onMouseDown={() => dragStart}
-        onMouseUp={() => dragStop}
-        onMouseMove={handleDrag}
-        transitionDuration={duration} // NOTE: for transitions
-        transitionEase={easingFunctions[ease]}
-        transitionBehavior={customAnimation ? scrollBehavior : undefined}
-      >
+      <div className="card-list" ref={scrollContainer.ref}>
         {data.map((item, key) => (
           <div
             key={key}
             className="content-card-btn"
-            onClick={handleItemClick(key, `/category/${item.title}`)}
+            onClick={handleItemClick(`/category/${item.title}`)}
           >
             <div className="card-info">{item.title}</div>
           </div>
         ))}
-      </ScrollMenu>
+      </div>
     </div>
   );
 }
