5b7fcc224d3473a266fb3d456ce7f5b46ffb11fa
epicX67 <souravgain605@gmail.com>
2023-03-15 01:24:54 +0530

    added square content

---
+++ src/components/CardSets/CardSetTypeSquare.js
@@ -0,0 +1,45 @@
+"use client";
+import { useScrollContainer } from "react-indiana-drag-scroll";
+import { useRouter } from "next/navigation";
+import { flags } from "@/res/data";
+import "./CardSetTypeSquare.scss";
+
+export default function CardSetTypeSquare({
+  title,
+  data = [],
+  call = () => {},
+}) {
+  const scrollContainer = useScrollContainer();
+  const router = useRouter();
+
+  const handleItemClick = (url) => () => {
+    router.push(url);
+    // Any required functions for specific page
+    call();
+  };
+
+  return (
+    <div className="content square">
+      <p>{title}</p>
+      <div ref={scrollContainer.ref} className="card-list">
+        {data.map(
+          (item, key) =>
+            item.squareImage && (
+              <div
+                key={key}
+                className="CardSetTypeA-card content-card"
+                style={{ backgroundImage: "url(" + item.squareImage + ")" }}
+                onClick={handleItemClick(`/game/${item.name.toLowerCase()}`)}
+              >
+                {Object.keys(flags).includes(item.flag) && (
+                  <img src={flags[item.flag]} alt="flag" />
+                )}
+
+                <div className="card-info">{item.name}</div>
+              </div>
+            )
+        )}
+      </div>
+    </div>
+  );
+}
