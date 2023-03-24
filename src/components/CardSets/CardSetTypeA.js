c191ee0425b9028bb51dc83560d0d079a8c80365
epicX67 <souravgain605@gmail.com>
2023-03-24 17:45:09 +0530

    some changes and enhancement done for typeA card

--- src/components/CardSets/CardSetTypeA.js
+++ src/components/CardSets/CardSetTypeA.js
@@ -21,20 +21,22 @@ export default function CardSetTypeA({ title, data = [], call = () => {} }) {
         {data.map(
           (item, key) =>
             item.wideImage && (
-              <div
-                key={key}
-                className="CardSetTypeA-card"
-                style={{ backgroundImage: "url(" + item.wideImage + ")" }}
-                onClick={handleItemClick(`/game/${item.name.toLowerCase()}`)}
-                onDrag={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                {Object.keys(flags).includes(item.flag) && (
-                  <img src={flags[item.flag]} alt="flag" />
-                )}
+              <div>
+                <div
+                  key={key}
+                  className="CardSetTypeA-card"
+                  style={{ backgroundImage: "url(" + item.wideImage + ")" }}
+                  onClick={handleItemClick(`/game/${item.name.toLowerCase()}`)}
+                  onDrag={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  {Object.keys(flags).includes(item.flag) && (
+                    <img src={flags[item.flag]} alt="flag" />
+                  )}
 
-                <div className="card-info">{item.name}</div>
+                  <div className="card-info">{item.name}</div>
+                </div>
               </div>
             )
         )}
