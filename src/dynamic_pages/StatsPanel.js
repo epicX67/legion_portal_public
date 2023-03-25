492cad9fea0d45cd3480937c5d6d02b82afe1bba
epicX67 <souravgain605@gmail.com>
2023-03-25 20:06:22 +0530

    revamped statpanel

--- src/dynamic_pages/StatsPanel.js
+++ src/dynamic_pages/StatsPanel.js
@@ -27,47 +27,64 @@ export default function StatsPanel({
     setStats(localGames.map((item) => games[nameOnly.indexOf(item)]));
   }, [games, type, localType]);
 
-  if (isEmbed) return null;
+  const handleClick = (itemName) => {
+    setState(false);
+    router.push(`/game/${itemName.toLowerCase()}`);
+  };
+
+  const closePanel = () => {
+    if (localType !== "") {
+      setLocalType("");
+    }
+    setState(false);
+  };
 
   return (
     state && (
       <div className="stats-panel">
-        <div className="resultList">
-          {stats.map((item, key) => (
-            <a
-              onClick={() => {
-                setState(false);
-                router.push(`/game/${item.name.toLowerCase()}`);
-              }}
-              key={key}
-              className="resultCard"
-            >
-              <div
-                className="card-logo"
-                style={{
-                  backgroundImage: "url(" + item.squareImage + ")",
-                }}
-              ></div>
-              <div className="card-title">{item.name}</div>
-            </a>
-          ))}
-          {stats.length === 0 && (
-            <div key="404NotFound" className="resultCardNotFound">
-              <i className="ri-emotion-sad-line"></i>
-              <div className="card-title">Nothing is there</div>
+        <div onClick={closePanel} className="touchBlocker"></div>
+        <div className="stat-container">
+          <div className="titleBar">
+            <div className="title">
+              {localType === "favourites" && "Your Favourites"}
+              {localType === "recents" && "Recently Played"}
+              {type !== "" && type}
             </div>
-          )}
+            <i className="ri-close-fill" onClick={() => closePanel()}></i>
+          </div>
+          <div className="resultList">
+            {stats.map((item, key) => (
+              <a key={key} className="resultCard">
+                <div
+                  className="card-logo"
+                  style={{
+                    backgroundImage: "url(" + item.squareImage + ")",
+                  }}
+                ></div>
+                <div
+                  className="card-title"
+                  onClick={() => handleClick(item.name)}
+                >
+                  {item.name}
+                </div>
+                <div className="stat-actions">
+                  <div
+                    onClick={() => handleClick(item.name)}
+                    className="action-btn light"
+                  >
+                    Play
+                  </div>
+                </div>
+              </a>
+            ))}
+            {stats.length === 0 && (
+              <div key="404NotFound" className="resultCardNotFound">
+                <i className="ri-emotion-sad-line"></i>
+                <div className="card-title">Nothing is there</div>
+              </div>
+            )}
+          </div>
         </div>
-        <button
-          onClick={() => {
-            if (localType !== "") {
-              setLocalType("");
-            }
-            setState(false);
-          }}
-        >
-          Close
-        </button>
       </div>
     )
   );
