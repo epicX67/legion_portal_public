7f32c5bb2316d3b453c847616dca4645cd469def
epicX67 <souravgain605@gmail.com>
2023-04-02 02:33:00 +0530

    new dynamic search route added to handle search

---
+++ src/app/(mainroot)/search/[name]/page.js
@@ -0,0 +1,16 @@
+import SearchClientWrapper from "../SearchClientWrapper";
+import "../styles.scss";
+
+async function getGames() {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/Xperiement/repo/main/sample.json`
+  );
+  const games = await res.json();
+  return games;
+}
+
+export default async function Categories({ params }) {
+  const games = await getGames();
+
+  return <SearchClientWrapper params={params} games={games} />;
+}
