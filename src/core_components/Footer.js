e35ab12410692d825d7e8971dfa1ec5f6bb20885
epicX67 <souravgain605@gmail.com>
2023-04-02 21:00:21 +0530

    enable footer after 3 second if it is in /game

--- src/core_components/Footer.js
+++ src/core_components/Footer.js
@@ -1,26 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
 import "./Footer.scss";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const [hide, setHide] = useState(true);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    if (pathName.includes("/game")) {
+      setHide(true);
+      setTimeout(() => {
+        setHide(false);
+      }, 3000);
+    } else {
+      setHide(false);
+    }
+  }, [pathName]);
+
   return (
-    <footer>
-      <div className="logo">
-        <img
-          className={`logo`}
-          src={
-            "https://cdn.discordapp.com/attachments/961665882671677493/1084136104409714688/logo.png"
-          }
-          alt=""
-        />
-        <div className={`logo-name`}>LEGiON Portal</div>
-      </div>
-      <div className="footer-links">
-        <div>About</div>
-        <div>Developers</div>
-        <div>Kids Edition</div>
-        <div>Terms</div>
-        <div>FAQ</div>
-        <div>Contact</div>
-      </div>
-    </footer>
+    !hide && (
+      <footer>
+        <div className="logo">
+          <img
+            className={`logo`}
+            src={
+              "https://cdn.discordapp.com/attachments/961665882671677493/1084136104409714688/logo.png"
+            }
+            alt=""
+          />
+          <div className={`logo-name`}>LEGiON Portal</div>
+        </div>
+        <div className="footer-links">
+          <div>About</div>
+          <div>Developers</div>
+          <div>Kids Edition</div>
+          <div>Terms</div>
+          <div>FAQ</div>
+          <div>Contact</div>
+        </div>
+      </footer>
+    )
   );
 }
